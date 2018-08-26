0fc8b6b353ca9f11ba8b52bca9e3d3e0bf7e90be
cutecprog <cutecprog@gmail.com>
2018-08-26 10:12:37 -0500

    do_not_run.py: Add missing backslash in next_step1 define statement and modify its string

--- do_not_run.py
+++ do_not_run.py
@@ -1,10 +1,11 @@
 from os import system
 # Globals
-next_step1 = 
-'''Good job so now you see why you need the system("pause"). Or you are this
-in do_not_run.py file in which case there's your hint.
-So now from don_not_run.py import next_step2 and print it out to you know like
-continue. IDK it's your life'''
+next_step1 = \
+'''Good job so now you see why you need the system("pause"). Or you are reading
+this in do_not_run.py file in which case there's your hint.
+
+So now from do_not_run.py import next_step2 and print it out to, you know like,
+continue, or whatever. IDK it's your life'''
 
 def main():
     print("I said don't run me! and don't open me up in notepad++ either. My complexity will confuse your simple mind.")
