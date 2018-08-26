ebef17d3d24c4d86d0e35701279e6f85fa6a3136
cutecprog <cutecprog@gmail.com>
2018-08-26 12:20:35 -0500

    Clean up do_not_run.py file

--- do_not_run.py
+++ do_not_run.py
@@ -1,31 +1,28 @@
 from os import system
-from random import randint
-# Globals
-next_step1 = \
-'''Good job so now you see why you need the system("pause"). Or you are reading
+
+def main():
+    print("I said don't run me! and don't open me up in notepad++ either. My complexity will confuse your simple mind.")
+    system("pause")
+# Sequence lesson
+next_step1 = '''
+Good job so now you see why you need the system("pause"). Or you are reading
 this in do_not_run.py file in which case there's your hint.
-
-So now from do_not_run.py import next_step2 and print it out to, you know like,
-continue, or whatever. IDK it's your life'''
-
-next_step2 = \
-'''Okay so you've proven you understand sequence (commands execute in order and
+    So now from the do_not_run.py file import next_step2 and print it out to,
+you know like, continue, or whatever. IDK it's your life'''
+# Calling functions lesson
+next_step2 = '''
+Okay so you've proven you understand sequence (commands execute in order and
 not all at once or something else). Now let's move on to something more confusing.
-Now you're going to call a function. I've already done this actually (see
+You're going to call a function. You've already done this actually (see
 print() and system() ) but now you're going to run a function with nothing
 between those parentheses (you call them arguments btw).
 So
 from do_not_run import next_step3
 then run it with
 next_step3()'''
-
-my_roll = randint(1,6)
-
-def main():
-    print("I said don't run me! and don't open me up in notepad++ either. My complexity will confuse your simple mind.")
-    system("pause")
-# Teach about calling functions
+# Workflow lesson
 def next_step3():
+    print("")
     print("Running next_step3...beep, boop")
     system("pause")
     print("All next_step* after this will be functions you run.")
@@ -34,6 +31,7 @@ def next_step3():
     system("pause")
 # Teach about seletion
 def next_step4():
+    print("")
     not_ready('If-else statements "lesson"')
     system("pause")
 # End of the line
