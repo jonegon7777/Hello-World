77f39e48997b36084da1ddfd79798b43233c532a
cutecprog <cutecprog@gmail.com>
2018-08-26 11:36:43 -0500

    Complete next_step2 string, and add my_roll list

--- do_not_run.py
+++ do_not_run.py
@@ -1,4 +1,5 @@
 from os import system
+from random import randint
 # Globals
 next_step1 = \
 '''Good job so now you see why you need the system("pause"). Or you are reading
@@ -7,7 +8,13 @@ this in do_not_run.py file in which case there's your hint.
 So now from do_not_run.py import next_step2 and print it out to, you know like,
 continue, or whatever. IDK it's your life'''
 
-next_step2 = "Wait for me to push something else. So like pull every once in a while to check"
+next_step2 = \
+'''Okay so you've proven you understand sequence (commands execute in order and
+not all at once or something else). Do let's move on to something more confusing.
+So I'm gonna give you a random number (1-6) in do_not_run.my_roll. Print out that
+variable.'''
+
+my_roll = [randint(1,6), "So I lied"]
 
 def main():
     print("I said don't run me! and don't open me up in notepad++ either. My complexity will confuse your simple mind.")
