f46dd5751eae34f3ef157eb7194fc1defceb8455
cutecprog <cutecprog@gmail.com>
2018-08-26 10:15:48 -0500

    do_not_run.py: add next_step2 global

--- do_not_run.py
+++ do_not_run.py
@@ -7,6 +7,8 @@ this in do_not_run.py file in which case there's your hint.
 So now from do_not_run.py import next_step2 and print it out to, you know like,
 continue, or whatever. IDK it's your life'''
 
+next_step2 = "Wait for me to push something else. So like pull every once in a while to check"
+
 def main():
     print("I said don't run me! and don't open me up in notepad++ either. My complexity will confuse your simple mind.")
     system("pause")
