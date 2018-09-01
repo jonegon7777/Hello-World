f734ce4091c2bf163176fce07922f04fc5920f5d
jonegon7777 <jon.e.gon7777@gmail.com>
2018-09-01 12:32:24 -0500

    i did stuph

--- run_this.py
+++ run_this.py
@@ -3,12 +3,13 @@
 print("Open me up in Notepad++")
 # Import the os library
 import os
-# Pause the application using a Window's basic command
-os.system("pause")
 # Import a string variable from do_not_run.py file
 from do_not_run import next_step1
 # Print next_step1 string (but there's a problem so fix it)
 print(next_step1)
+# Pause the application using a Window's basic command
+os.system("pause")
+
 
 # Edit this file however you see fit
 # Also feel free to add new *.py files to the repo
